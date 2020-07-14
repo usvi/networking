4a80e7ccbfdc4357d88e96d3aeb29cad6345f593
Janne Paalijarvi <jpaalija@gmail.com>
2020-07-15 01:07:32 +0300

    Changed virtual mac addresses

--- dhcp-on-multiple-virtual-interfaces-dhcpcd/usr/local/sbin/interfaces_init.sh
+++ dhcp-on-multiple-virtual-interfaces-dhcpcd/usr/local/sbin/interfaces_init.sh
@@ -8,11 +8,11 @@ rm -rf "$SCRIPTS_LOCKDIR"
 
 # Create the virtual interfaces
 ifconfig "$IF_VIRTUAL_BASE" up
-ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:10:5b "$IF_PUB0" type macvlan
+ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:20:5b "$IF_PUB0" type macvlan
 ip link set "$IF_PUB0" up
-ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:11:5b "$IF_PUB1" type macvlan
+ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:21:5b "$IF_PUB1" type macvlan
 ip link set "$IF_PUB1" up
-ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:12:5b "$IF_PUB2" type macvlan
+ip link add link "$IF_VIRTUAL_BASE" address 00:90:0b:ff:22:5b "$IF_PUB2" type macvlan
 ip link set "$IF_PUB2" up
 
 # Enable forwarding
