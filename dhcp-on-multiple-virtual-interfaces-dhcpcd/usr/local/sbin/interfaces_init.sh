440da12b129f1b3c9d089b52d0a32cd3d2f64730
root <root@gw.local>
2020-10-22 09:39:00 +0300

    Stuff seems to be working also without ethtool stuff

--- dhcp-on-multiple-virtual-interfaces-dhcpcd/usr/local/sbin/interfaces_init.sh
+++ dhcp-on-multiple-virtual-interfaces-dhcpcd/usr/local/sbin/interfaces_init.sh
@@ -9,10 +9,10 @@ rm -rf "$SCRIPTS_LOCKDIR"
 # Create the virtual interfaces
 ip link set "$IF_VIRTUAL_BASE" up
 
-ethtool --offload "$IF_VIRTUAL_BASE" rx off tx off
-ethtool --offload enp3s0 rx off tx off
+#ethtool --offload "$IF_VIRTUAL_BASE" rx off tx off
+#ethtool --offload enp3s0 rx off tx off
 
-ip link set "$IF_VIRTUAL_BASE" promisc on
+#ip link set "$IF_VIRTUAL_BASE" promisc on
 ip link add link "$IF_VIRTUAL_BASE" address 00:91:2e:4d:43:67 "$IF_PUB0" type macvlan
 ip link set "$IF_PUB0" up
 ip link add link "$IF_VIRTUAL_BASE" address 00:92:2e:4d:43:67 "$IF_PUB1" type macvlan
